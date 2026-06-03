9965235f507d540e1a12007d62f31f6afdc7ee77
Jonah Aragon <jonah@triplebit.net>
2026-06-03 12:27:00 -0500

    Add license for external database

--- app/src/main/kotlin/org/privacyguides/verifiedapps/ui/CreditsScreen.kt
+++ app/src/main/kotlin/org/privacyguides/verifiedapps/ui/CreditsScreen.kt
@@ -249,11 +249,42 @@ private val ISC_LICENSE_SOUPSURPR_ROUNDSALMON =
         "Copyright (c) 2024-2025 soupslurpr\nCopyright (c) 2026 RoundSalmon4",
     )
 
+private const val MIT_LICENSE =
+    "\nMIT License\n" +
+        "\n" +
+        "Copyright (c) 2026 Jonah Aragon\n" +
+        "Copyright (c) 2026 Privacy Guides & contributors\n" +
+        "\n" +
+        "Permission is hereby granted, free of charge, to any person obtaining a copy " +
+        "of this software and associated documentation files (the \"Software\"), to deal " +
+        "in the Software without restriction, including without limitation the rights " +
+        "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell " +
+        "copies of the Software, and to permit persons to whom the Software is " +
+        "furnished to do so, subject to the following conditions:\n" +
+        "\n" +
+        "The above copyright notice and this permission notice shall be included in all " +
+        "copies or substantial portions of the Software.\n" +
+        "\n" +
+        "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR " +
+        "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, " +
+        "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE " +
+        "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER " +
+        "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, " +
+        "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE " +
+        "SOFTWARE."
+
 @Composable
 fun CreditsScreen() {
     LazyColumn(
         modifier = Modifier
     ) {
+        item {
+            CreditsItem(
+                dependencyName = "Verified Apps Database",
+                dependencyPackageName = "Crowdsourced Android app signature data",
+                dependencyLicense = MIT_LICENSE,
+            )
+        }
         item {
             CreditsItem(
                 dependencyName = "AppVerifier",
